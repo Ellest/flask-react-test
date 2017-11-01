91ebed10d41158167461c83fb0c30790824c9eeb
Ellest <syunelli@gmail.com>
2017-10-31 23:41:11 -0500

    docker setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify
+import os
 
 # instantiate app
 app = Flask(__name__)
 
+# pull in environment vars
+app_settings = os.getenv('APP_SETTINGS')
 # set config from config file (config.py)
 app.config.from_object('app.config.DevelopmentConfig')
 
